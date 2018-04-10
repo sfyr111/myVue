99871312e5a60d90cab92aa3951c3e34baaa09eb
sfyr111 <sfyr111@163.com>
2018-04-10 22:53:33 +0800

    依赖收集

--- index.js
+++ index.js
@@ -1,13 +1,89 @@
-import Vue from './vue'
 
+class Dep {
+  constructor() {
+    this.subs = []
+  }
 
-var test = {
-  a: 1,
-  b: 2
+  addSub(sub) {
+    this.subs.push(sub)
+  }
+
+  notify() {
+    this.subs.forEach(sub => {
+      sub.update()
+    })
+  }
 }
 
-var vm = new Vue({
-  data: test
+class Watcher {
+  constructor() {
+    Dep.target = this
+  }
+
+  update() {
+    console.log('watch update')
+  }
+}
+
+/**
+ *
+ * @param value {options.data}
+ */
+function observer(value) {
+  if (!value || (typeof value !== 'object')) {
+    return
+  }
+
+  Object.keys(value).forEach(key => {
+    defineReactive(value, key, value[key])
+  })
+}
+
+function defineReactive(obj, key, val) {
+  const dep = new Dep()
+
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get: function reactiveGetter() {
+      dep.addSub(Dep.target)
+      return val
+    },
+    set: function reactiveSetter(newVal) {
+      if (newVal === val) return
+      val = newVal
+      dep.notify()
+    }
+  })
+}
+
+class Vue {
+  constructor(options) {
+    this._data = options.data
+    observer(this._data)
+    new Watcher()
+    console.log('<div>this._data.test</div>视图绑定 1 次', this._data.test)
+    console.log('<div>this._data.test</div>视图绑定 2 次', this._data.test)
+    console.log('<div>this._data.test</div>视图绑定 3 次', this._data.test)
+    console.log('<div>this._data.test</div>视图绑定 4 次', this._data.test)
+    console.log('<div>this._data.test</div>视图绑定 5 次', this._data.test)
+
+    console.log('<div>this._data.AAA</div>视图绑定 1 次', this._data.AAA)
+    console.log('<div>this._data.AAA</div>视图绑定 2 次', this._data.AAA)
+    console.log('<div>this._data.AAA</div>视图绑定 3 次', this._data.AAA)
+  }
+}
+
+
+let vm = new Vue({
+  data: {
+    test: 'i am a test',
+    AAA: 'i am a AAA',
+  }
 })
 
-test.a = 3
+
+vm._data.test = '更新 test'
+vm._data.AAA = '更新 AAA'
+
+Dep.target = null
